5e83229c0f029a75da2bbb4902e13feaac3b2234
Luzaks <kanemekanik@gmail.com>
2020-06-08 20:36:17 -0500

    Feat: Add motion designs for projects.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import anime from 'animejs/lib/anime.es.js';
 import rallax from 'rallax.js';
 import AOS from 'aos';
 import lax from 'lax.js'
 
+anime({
+    targets: '.dev-u',
+    rotate: '1turn',
+    duration: 800,
+    scale: 1.1,
+});
+
+anime({
+    targets: '.half-background',
+    border: '100%', // -> from '28px' to '100%',
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+});
 
 const target = document.querySelector('.parallax');
+
 const parallax = rallax(target, { speed: 0.5 });
 parallax.when(
     () => window.scrollY > 1000,
