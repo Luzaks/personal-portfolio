cfe55ba57102110c6c785f9311dd0bde996aa702
Luzaks <kanemekanik@gmail.com>
2020-08-13 16:18:26 -0500

    Feat: Add animations to images

--- src/components/main/portfolio.js
+++ src/components/main/portfolio.js
@@ -5,14 +5,14 @@ import { PortfolioCont, AboutMeCont } from '../../styles/StyledComponents';
 const Portfolio = () => {
   return(
     <PortfolioCont>
-      <AboutMeCont>
-        <h1>
-          Luzaks
-        </h1>
-        <span>
-          portfolio
-        </span>
-      </AboutMeCont>
+        <AboutMeCont>
+            <h1>
+              Luzaks
+            </h1>
+            <span>
+              portfolio
+            </span>
+        </AboutMeCont>
       <Scroller />
     </PortfolioCont>
   );
--- src/components/scroller/scroller.js
+++ src/components/scroller/scroller.js
@@ -1,52 +1,25 @@
-import React, {useRef, useEffect} from 'react';
-import useWindowSize from '../../hooks/useWindowSize';
+import React, {useEffect} from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import images from '../../images/images';
-import { ScrollerCont, ScrollApp } from '../../styles/StyledComponents';
+import { ScrollerCont } from '../../styles/StyledComponents';
 
 const Scroller = () => {
-    const size = useWindowSize();
-    const app = useRef();
-    const scrollContainer = useRef();
-
-    const skewConfigs =  {
-        ease: .1,
-        current: 0,
-        previous:0,
-        rounded: 0
-    };
-
-    useEffect(() => {
-        document.body.style.height= `${
-            scrollContainer.current.getBoundingClientRect().height
-        }px`
-    }, [size.height]);
-
-    useEffect(() => {
-        requestAnimationFrame(() => skewScrolling());
-    }, []);
-
-    const skewScrolling = () => {
-        skewConfigs.current = window.scrollY;
-        skewConfigs.previous += (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
-        skewConfigs.rounded = Math.round(skewConfigs.previous * 100) /100;
-
-        const difference = skewConfigs.current - skewConfigs.rounded;
-        const acceleration = difference / size.width;
-        const velocity = +acceleration;
-        const skew = velocity + 7.5;
-
-        scrollContainer.current.style.transform = `translate3d(0, ${skewConfigs.rounded}px, 0) skewY(${skew})deg`;
-
-        requestAnimationFrame(() => skewScrolling());
-    };
-
+  useEffect(() => {
+    Aos.init({
+      duration: 2000
+    });
+  }, []);
     return(
-    <ScrollApp ref={app}>
-      <ScrollerCont ref={scrollContainer} >
+      <ScrollerCont >
         {images.map((image, index) => (
           <>
             <div key={index} className='img-container'>
-              <img src={image} alt={`people ${index}`} />
+              <img
+                  data-aos="fade-up"
+                  data-aos-mirror="true"
+                  src={image}
+                  alt={`project ${index}`} />
             </div>
             <h2>
               Skew <span className='outline'>Scrolling</span>
@@ -54,7 +27,6 @@ const Scroller = () => {
           </>
         ))}
       </ScrollerCont>
-    </ScrollApp>
   );
 };
 
