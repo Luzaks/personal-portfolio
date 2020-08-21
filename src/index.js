3f06d1eded3fcb93bcb9c47906da23e82a66a813
Luzaks <kanemekanik@gmail.com>
2020-08-21 16:33:54 -0500

    Feat: Exctract logic from index.js.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
-import React, { Suspense, useEffect, useRef, useMemo } from 'react';
-import { Canvas, Dom, useLoader, useFrame } from 'react-three-fiber';
-import { TextureLoader, LinearFilter } from 'three';
+import React, { Suspense, useEffect, useRef } from 'react';
+import { Canvas, Dom, useFrame } from 'react-three-fiber';
 import lerp from 'lerp';
+import Content from './components/Content';
 import { Text } from './components/Text';
 import Plane from './components/Plane';
 import { Block, useBlock } from './blocks';
@@ -41,50 +41,6 @@ function Paragraph({ image, index, offset, factor, header, aspect, text }) {
                 </Block>
             </group>
         </Block>
-    )
-}
-
-function Content() {
-    const images = useLoader(
-        TextureLoader,
-        state.paragraphs.map(({ image }) => image)
-    )
-    useMemo(() => images.forEach(texture => (texture.minFilter = LinearFilter)), [images]);
-    const { contentMaxWidth: w, canvasWidth, canvasHeight, mobile } = useBlock();
-    return (
-        <>
-            <Block factor={1} offset={0}>
-                <Block factor={1.2}>
-                    <Text left size={w * 0.08} position={[-w / 3.2, 0.5, -1]} color="#d40749">
-                        LUZAKS
-                    </Text>
-                </Block>
-                <Block factor={1.0}>
-                    <Dom position={[-w / 4.5, -w * 0.08 + 0.25, -1]}>FULL STACK DEVELOPER{mobile ? <br /> : " "}PORTFOLIO</Dom>
-                </Block>
-            </Block>
-            <Block factor={1.2} offset={6}>
-                <Dom>
-                    hello color="#2fe8c3"
-                </Dom>
-                <Dom className="bottom-left" position={[-canvasWidth / 2, -canvasHeight / 2, 0]}>
-                    Hay que realizar todos tus sueños.
-                </Dom>
-            </Block>
-            <Block factor={1.4} offset={7}>
-                <Dom center>
-                    hello
-                </Dom>
-            </Block>
-            {state.paragraphs.map((props, index) => (
-                <Paragraph key={index} index={index} {...props} image={images[index]} />
-            ))}
-            {state.stripes.map(({ offset, color, height }, index) => (
-                <Block key={index} factor={-1.5} offset={offset}>
-                    <Plane args={[50, height, 32, 32]} shift={-4} color={color} rotation={[0, 0, Math.PI / 8]} position={[0, 0, -10]} />
-                </Block>
-            ))}
-        </>
     )
 }
 
