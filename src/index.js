b8bcb7a4dfeb014c848648cb2888b09ce6cecdd9
Luzaks <kanemekanik@gmail.com>
2020-08-21 15:16:40 -0500

    Feat: Refactor code on index file.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,136 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense, useEffect, useRef, useMemo } from 'react';
+import { Canvas, Dom, useLoader, useFrame } from 'react-three-fiber';
+import { TextureLoader, LinearFilter } from 'three';
+import lerp from 'lerp';
+import { Text } from './components/Text';
+import Plane from './components/Plane';
+import { Block, useBlock } from './blocks';
+import state from './store';
+import './styles.css';
 
-ReactDOM.render(
-  <ParallaxProvider>
-    <App />
-  </ParallaxProvider>,
-  document.getElementById('root')
-);
+function Startup() {
+    const ref = useRef();
+    useFrame(() => (ref.current.material.opacity = lerp(ref.current.material.opacity, 0, 0.025)));
+    return <Plane ref={ref} color="#0e0e0f" position={[0, 0, 200]} scale={[100, 100, 1]} />
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Paragraph({ image, index, offset, factor, header, aspect, text }) {
+    const { contentMaxWidth: w, canvasWidth, margin, mobile } = useBlock();
+    const size = aspect < 1 && !mobile ? 0.65 : 1;
+    const alignRight = (canvasWidth - w * size - margin) / 2;
+    const pixelWidth = w * state.zoom * size;
+    const left = !(index % 2);
+    const color = index % 2 ? "#D40749" : "#2FE8C3";
+    return (
+        <Block factor={factor} offset={offset}>
+            <group position={[left ? -alignRight : alignRight, 0, 0]}>
+                <Plane map={image} args={[1, 1, 32, 32]} shift={75} size={size} aspect={aspect} scale={[w * size, (w * size) / aspect, 1]} frustumCulled={false} />
+                <Dom
+                    style={{ width: pixelWidth / (mobile ? 1 : 2), textAlign: left ? "left" : "right" }}
+                    position={[left || mobile ? (-w * size) / 2 : 0, (-w * size) / 2 / aspect - 0.4, 1]}>
+                    <div tabIndex={index}>{text}</div>
+                </Dom>
+                <Text left={left} right={!left} size={w * 0.04} color={color} top position={[((left ? -w : w) * size) / 2, (w * size) / aspect / 2 + 0.5, -1]}>
+                    {header}
+                </Text>
+                <Block factor={0.2}>
+                    <Text opacity={0.5} size={w * 0.1} color="#1A1E2A" position={[((left ? w : -w) / 2) * size, (w * size) / aspect / 1.5, -10]}>
+                        {"0" + (index + 1)}
+                    </Text>
+                </Block>
+            </group>
+        </Block>
+    )
+}
+
+function Content() {
+    const images = useLoader(
+        TextureLoader,
+        state.paragraphs.map(({ image }) => image)
+    )
+    useMemo(() => images.forEach(texture => (texture.minFilter = LinearFilter)), [images]);
+    const { contentMaxWidth: w, canvasWidth, canvasHeight, mobile } = useBlock();
+    return (
+        <>
+            <Block factor={1} offset={0}>
+                <Block factor={1.2}>
+                    <Text left size={w * 0.08} position={[-w / 3.2, 0.5, -1]} color="#d40749">
+                        LUZAKS
+                    </Text>
+                </Block>
+                <Block factor={1.0}>
+                    <Dom position={[-w / 3.2, -w * 0.08 + 0.25, -1]}>It was the year 2076.{mobile ? <br /> : " "}The substance had arrived.</Dom>
+                </Block>
+            </Block>
+            <Block factor={1.2} offset={6}>
+                <Dom>
+                    hello color="#2fe8c3"
+                </Dom>
+                <Dom className="bottom-left" position={[-canvasWidth / 2, -canvasHeight / 2, 0]}>
+                    Culture is not your friend.
+                </Dom>
+            </Block>
+            <Block factor={1.4} offset={7}>
+                <Dom center>
+                    hello
+                </Dom>
+            </Block>
+            {state.paragraphs.map((props, index) => (
+                <Paragraph key={index} index={index} {...props} image={images[index]} />
+            ))}
+            {state.stripes.map(({ offset, color, height }, index) => (
+                <Block key={index} factor={-1.5} offset={offset}>
+                    <Plane args={[50, height, 32, 32]} shift={-4} color={color} rotation={[0, 0, Math.PI / 8]} position={[0, 0, -10]} />
+                </Block>
+            ))}
+        </>
+    )
+}
+
+function App() {
+    const scrollArea = useRef();
+    const onScroll = e => (state.top.current = e.target.scrollTop);
+    useEffect(() => void onScroll({ target: scrollArea.current }), []);
+    return (
+        <>
+            <Canvas className="canvas" concurrent pixelRatio={1} orthographic camera={{ zoom: state.zoom, position: [0, 0, 500] }}>
+                <Suspense fallback={<Dom center className="loading" children="Loading..." />}>
+                    <Content />
+                    <Startup />
+                </Suspense>
+            </Canvas>
+            <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+                {new Array(state.sections).fill().map((_, index) => (
+                    <div key={index} id={"0" + index} style={{ height: `${(state.pages / state.sections) * 100}vh` }} />
+                ))}
+            </div>
+            <div className="frame">
+                <h1 className="frame__title">Guadalupe Rangel</h1>
+                <div className="frame__links">
+                    <a className="frame__link" href="http://tympanus.net/Tutorials/PhysicsMenu/">
+                        Previous demo
+                    </a>
+                    <a className="frame__link" href="https://tympanus.net/codrops/?p=45441">
+                        Article
+                    </a>
+                    <a className="frame__link" href="https://github.com/drcmda/the-substance">
+                        GitHub
+                    </a>
+                </div>
+                <div className="frame__nav">
+                    <a className="frame__link" href="#00" children="intro" />
+                    <a className="frame__link" href="#01" children="01" />
+                    <a className="frame__link" href="#02" children="02" />
+                    <a className="frame__link" href="#03" children="03" />
+                    <a className="frame__link" href="#04" children="04" />
+                    <a className="frame__link" href="#05" children="05" />
+                    <a className="frame__link" href="#06" children="06" />
+                    <a className="frame__link" href="#07" children="07" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
